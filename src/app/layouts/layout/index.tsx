bb35600ee486bc78755aea8878adf00a552509e3
Dennis Weru <officialweru@gmail.com>
2026-04-29 13:17:19 +0300

    fix: remove missing Hero import and resolve build errors

--- src/app/layouts/layout/index.tsx
+++ src/app/layouts/layout/index.tsx
@@ -3,9 +3,5 @@ import React from 'react'
 export const getNoneLayout = (page: React.ReactElement) => page
 
 export const getDefaultLayout = (page: React.ReactElement) => {
-  return (
-    <div className="min-h-screen bg-white">
-      {page}
-    </div>
-  )
+  return <div className="min-h-screen bg-white">{page}</div>
 }
--- src/app/pages/home/index.tsx
+++ src/app/pages/home/index.tsx
@@ -1,11 +1,5 @@
-import { Helmet } from 'react-helmet-async'
-import { useTranslation } from 'react-i18next'
-import { Hero } from '../../../shared/components/hero'
+import React from 'react'
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center">
-      {/* Starting point for the agent */}
-    </div>
-  )
+  return <div className="flex min-h-screen items-center justify-center">{/* Starting point for the agent */}</div>
 }
